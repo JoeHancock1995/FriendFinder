b8d0ac41555430c5d99d648aa606f66e123086cc
Joe Hancock <joe.hancock95@gmail.com>
2019-09-02 20:03:43 -0500

    apiroute work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,28 @@ app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-//==================POSt requests====================//
+//==================POST requests====================//
 // create a POST routes with /api/friends this will handle incoming survey results. also handles compatibility logic
 app.post("/api/friends", function(req, res) {
     
+  var bestMatch = {
+    name: "",
+    photo: "",
+    difference: Infinity
+  };
+
+  var friendData = req.body;
+  var total = friendData.scores;
+  var totalDifference;
+
+  for (var i = 0; i < friends.length; i++) {
+    var currentUser = friends[i];
+    totalDifference = 0;
+
+    console.log(currentUser.name);
+
+    for (var j= 0; j = currentUser.name);
+  }
+
 }) 
 };
